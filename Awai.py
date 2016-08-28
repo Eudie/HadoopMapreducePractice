f56640ddf3c24798e4e7ad5c9ea79a33714c134d
Eudie <utkarsh.dubey.26@gmail.com>
2016-08-28 21:26:06 +0530

    first draft

---
+++ Awai.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 28 21:15:27 2016
+
+@author: Eudie
+"""
+
+a  = 1
---
+++ FirstMapReduce.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 28 21:11:28 2016
+
+@author: Eudie
+"""
+
+wahtjhh
