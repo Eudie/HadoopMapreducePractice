03b4903d96fa551ee6c65535ca0b0e1cea90a92e
Eudie <utkarsh.dubey.26@gmail.com>
2016-10-12 17:44:03 +0530

    Completed DTM creation

---
+++ M_0_news.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Mapper to create DTM from csv as input
+
+import sys
+#import io
+import re
+import csv
+#from nltk.stem import SnowballStemmer
+#from nltk.corpus import stopwords
+#stop_words = set(stopwords.words('english'))
+#snowball_stemmer = SnowballStemmer('english')
+
+WORD_RE = re.compile(r"[\w']+")
+for line in sys.stdin:
+    data = line.strip().split(',')
+    key = data[1]
+    news = data[9]
+    for word in re.split(' |\r\n', news):
+        if word is not None or word[0] not in ["#", "@"]:
+            if word[:4] != "http":
+                word = word.lower()
+                for FinalWord in WORD_RE.findall(word):
+                    #FinalWord = snowball_stemmer.stem(FinalWord)
+                    #if FinalWord not in stop_words:
+                    print "{0}\t{1}".format(key, FinalWord)
--- reducer.py
+++ reducer.py
@@ -7,15 +7,15 @@ Total = 0
 oldWord = None
 
 for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
+    data_mapped = line.strip().split('\t')
     if len(data_mapped) != 2:
         # Something has gone wrong. Skip this line.
         continue
 
-    ID, tweetWord = data_mapped
-    NewWord = ID + " | " + tweetWord
+    ID_tweetWord, no = data_mapped
+    NewWord = ID_tweetWord
     if oldWord and oldWord != NewWord:
-        print oldWord, "\t", Total
+        print oldWord, '\t', Total
         oldWord = NewWord
         Total = 0
 
@@ -23,4 +23,4 @@ for line in sys.stdin:
     Total += 1
 
 if oldWord is not None:
-    print oldWord, "\t", Total
+    print oldWord, '\t', Total
