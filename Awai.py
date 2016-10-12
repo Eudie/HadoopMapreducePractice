0170ef6dbfb9fa67f01e3fb09f7218c433b3dcab
Eudie <utkarsh.dubey.26@gmail.com>
2016-10-12 17:54:28 +0530

    remove files

--- Awai.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 28 21:15:27 2016
-
-@author: Eudie
-"""
-
-a  = 1
--- FirstMapReduce.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 28 21:11:28 2016
-
-@author: Eudie
-"""
-
-wahtjhh
